5d6c0253ddfec116c16b4d56a50755c73007ba9a
bmmors <bmmors@gmail.com>
2017-02-23 21:55:06 -0800

    Version 3
    Human Control working ... ish

--- Homework2.cpp
+++ Homework2.cpp
@@ -14,16 +14,16 @@ using namespace std;
 
 class grid {
 public:
-	int xGS = 0; //set x value for grid size
-	int yGS = 0; //set y calue for grid size
-	int Ax = 0;
-	int Ay = 0;
-	int Gx = 0;
-	int Gy = 0;
-
+	int xGS = 10; //set x value for grid size
+	int yGS = 10; //set y calue for grid size
+	int Ax = rand() % xGS; //Randomly assign agent new grid location
+	int Ay = rand() % yGS;
+	int Gx = rand() % xGS; //Randomly assign goal new grid location
+	int Gy = rand() % yGS;
+	char action = 'a';
 	char **fullgrid;
 
-	grid(int x,int y);
+	void build_grid();
 	void init();
 	void print();
 	void update_agent();
@@ -33,8 +33,7 @@ public:
 
 };
 
-grid::grid(int x,int y) {
-	xGS = x; yGS = y;
+void grid::build_grid() {
 	fullgrid = new char*[xGS]();
 	for (int i = 0; i < xGS; i++) {
 		fullgrid[i] = new char[yGS];
@@ -42,11 +41,10 @@ grid::grid(int x,int y) {
 }
 
 void grid::init() {
-	Ax = rand() % xGS;
-	Ay = rand() % yGS;
-	Gx = rand() % xGS;
-	Gy = rand() % yGS;
+	
+}
 
+void grid::print() {
 	for (int i = 0; i < xGS; i++) {
 		for (int j = 0; j < yGS; j++) {
 			if (i == Ax && j == Ay) {
@@ -58,28 +56,84 @@ void grid::init() {
 			else {
 				fullgrid[i][j] = '-';
 			}
-			
+
 		}
 	}
-}
-
-void grid::print() {
+	system("cls");
 	for (int i = 0; i < xGS; i++) {
 		for (int j = 0; j < yGS; j++) {
-			cout << fullgrid[i][j];
+			cout << fullgrid[i][j] << "\t";
+		}
+		cout << endl;
+	}
+}
+
+void grid::update_agent() {
+	if (action == 'u') {
+		if (Ax > xGS || Ax <= 0) {
+			cout << "Invalid Move\n";
+		}
+		else {
+			Ax -= 1;
 		}
 	}
-
+	else if (action == 'd') {
+		if (Ax > xGS || Ax <= 0) {
+			cout << "Invalid Move\n";
+		}
+		else {
+			Ax += 1;
+		}
+	}
+	else if (action == 'r') {
+		if (Ay > yGS || Ay <= 0) {
+			cout << "Invalid Move\n";
+		}
+		else {
+			Ay += 1;
+		}
+	}
+	else if (action == 'l') {
+		if (Ay > yGS || Ay <= 0) {
+			cout << "Invalid Move\n";
+		}
+		else {
+			Ay -= 1;
+		}
+	}
 }
+
+
 void grid::clear() {
 	delete[] fullgrid;
 }
 
 int main() {
-	int Ax = 0; //initialize agent location at (0,0)
-	int Ay = 0;
-	int Gx = 0; //initialize goal location at (0,0)
-	int Gy = 0;
+	grid G;
+	G.build_grid(); //build grid array
+	G.init(); //place A and G in random locations and fill grid array
+	int gameplay = 0; //variable for selecting gameplay option
+	cout << "Select a Gameplay Option:\n1.Human Controlled\n2.Autosolve" << endl;
+	cin >> gameplay;
+	if (gameplay == 1) {
+		bool gamestatus = true; //variable for gameplay loop
+		while (gamestatus == true) {
+			G.print();
+			if (G.Ax == G.Gx && G.Ay == G.Gy) {
+				cout << "WINNER!!\n";
+				gamestatus = false;
+			}
+			else {
+				cout << "Choose a direction to move the agent.\nEnter U for up, D for down, R for Right, or L for left\n";
+				cin >> G.action;
+				G.update_agent();
+			}
+		}
+	}
+	else if (gameplay == 2) {
 
+	}
+		
+	G.clear();
 	return 0;
 }
