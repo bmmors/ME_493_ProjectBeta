2593d4f9f54a1ba508255f23d2dbf55f381ced24
bmmors <bmmors@gmail.com>
2017-03-02 18:51:42 -0800

    Version 1
    Qlearner working ... i think

--- Mors_Britny_ProjectBeta.cpp
+++ Mors_Britny_ProjectBeta.cpp
@@ -34,82 +34,147 @@ public:
 	int action;
 	int state;
 
-	vector< vector<double> > qtable;
-
-	void init(int gridx, int gridy);
-	int get_state(int gridx);
+	void update(int move, int gridx, int gridy);
 };
 
-void agent::init(int gridx, int gridy) {
-	for (int i = 0; i < (gridx*gridy); i++) {
-		vector<double> row;
-		for (int j = 0; j < 4; j++) {
-			row.push_back(LYRAND);
+class qlearner {
+public:
+	double Qval;
+	double Qmax;
+	int reward;
+	double alpha = 0.1;
+	double gamma = 0.9;
+	double epsilon;
+	int action;
+	int state;
+	int newstate;
+	vector< vector<double> > qtable;
+	vector< vector<int> > rtable;
+	void init(int gridx, int gridy, int goalx, int goaly);
+	void sense(int agentx, int agenty, int x_dim);
+	void decide();
+	int act();
+	void react(int agentx, int agenty, int x_dim);
+
+};
+
+void agent::update(int move, int gridx, int gridy) {
+	if (move == 0) {
+		if (ax > gridx || ax <= 0) {
+			ax = ax;
 		}
-		qtable.push_back(row);
+		else {
+			ax -= 1;
+		}
 	}
-	ax = Ax_start;
-	ay = Ay_start;
-	state = get_state(gridx);
-	action = 5;
+	else if (move == 1) {
+		if (ax >= gridx-1 || ax < 0) {
+			ax = ax;
+		}
+		else {
+			ax += 1;
+		}
+
+	}
+	else if (move == 2) {
+		if (ay > gridx || ax <= 0) {
+			ay = ay;
+		}
+		else {
+			ay -= 1;
+		}
+	}
+	else if (move == 3) {
+		if (ay >= gridx - 1 || ay < 0) {
+			ay = ay;
+		}
+		else {
+			ay += 1;
+		}
+	}
 }
 
-int agent::get_state(int gridx) {
-	return ax + (ay*gridx);
+void qlearner::init(int gridx, int gridy, int goalx, int goaly) {
+	//build reward table
+	for (int i = 0; i < (gridx); i++) {
+		vector<int> rrow;
+		for (int j = 0; j < gridy; j++) {
+			if (i == goalx && j == goaly) {
+				rrow.push_back(100);
+			}
+			else {
+				rrow.push_back(-1);
+			}
+		}
+		rtable.push_back(rrow);
+	}
+	//build qtable
+	for (int i = 0; i < (gridx); i++) {
+		vector<double> qrow;
+		for (int j = 0; j < gridy; j++) {
+			qrow.push_back(LYRAND);
+		}
+		qtable.push_back(qrow);
+	}
 }
 
-/*
-void grid::human_update_agent() {
-	if (action == 'u') {
-		if (Ax > xGS || Ax <= 0) {
-			cout << ".";
-		}
-		else {
-			Ax -= 1;
-		}
+void qlearner::sense(int agentx, int agenty,int x_dim) {
+	state = agentx + (agenty*x_dim);
+	reward = rtable[agentx][agenty];
+}
+
+void qlearner::decide() {
+	epsilon = LYRAND;
+	if (epsilon <= 0.1) {
+		action = rand() % 4; //random option
 	}
-	else if (action == 'd') {
-		if (Ax >= xGS - 1 || Ax < 0) {
-			cout << "Invalid Move\n";
-		}
-		else {
-			Ax += 1;
-		}
-	}
-	else if (action == 'r') {
-		if (Ay >= yGS - 1 || Ay < 0) {
-			cout << "Invalid Move\n";
-		}
-		else {
-			Ay += 1;
-		}
-	}
-	else if (action == 'l') {
-		if (Ay > yGS || Ay <= 0) {
-			cout << "Invalid Move\n";
-		}
-		else {
-			Ay -= 1;
+	else {
+		int test = qtable[state][0];
+		action = 0;
+		for (int i = 0; i < 4; i++) {
+			if (test < qtable[state][i]) {
+				action = i;
+				test = qtable[state][i];
+			}
 		}
 	}
 }
-*/
+
+int qlearner::act() {
+	//action list:
+	//0 = up
+	//1 = down
+	//2 = right
+	//3 = left
+	Qval = qtable[state][action];
+	return action;
+}
+
+void qlearner::react(int agentx, int agenty, int x_dim) {
+	newstate = agentx + (agenty*x_dim);
+	int w = qtable[newstate][0]; //save first int in vector
+	for (int i = 0; i < 4; i++) {
+		if (w < qtable[newstate][i]) {
+			Qmax = qtable[newstate][i]; //save the new max value in qmax
+			w = qtable[newstate][i]; //reset w with higest number
+		}
+		qtable[newstate][action] = Qval + alpha*(reward + (gamma*Qmax) - Qval);
+	}
+}
 
 int main() {
 	grid G;
 	agent A;
-	A.init(G.xGS, G.yGS);
-	cout << "Grid	x:" << G.xGS << endl;
-	cout << "Grid	y:" << G.yGS << endl;
-	cout << "Agent	x:" << A.ax << endl;
-	cout << "Agent	y:" << A.ay << endl;
-	cout << "State	 :" << A.state << endl;
-	cout << "Qtable" << endl;
-	for (int i = 0; i < (G.xGS*G.yGS); i++) {
-		for (int j = 0; j < 4; j++) {
-			cout << A.qtable[i][j] << "\t" << endl;
+	qlearner Q;
+	Q.init(G.xGS,G.yGS,G.Gx,G.Gy);
+	for (int i=0; i < 30; i++)
+		for (int j = 0; j < 1000; j++) {
+			Q.sense(A.ax, A.ay, G.xGS);
+			Q.decide();
+			A.update(Q.act(), G.xGS, G.yGS);
+			Q.react(A.ax, A.ay, G.xGS);
 		}
-	}
 
+	
 	return 0;
 }
